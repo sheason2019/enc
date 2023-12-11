837b370962ecf573f09548169bab4743c79e62ae
sheason <811627877@qq.com>
2023-12-11 14:28:44 +0800

    feat: 正确展示 Conversation 的名称

--- flutter/lib/chat/room/title/title.view.dart
+++ flutter/lib/chat/room/title/title.view.dart
@@ -1,3 +1,4 @@
+import 'package:drift/drift.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:sheason_chat/extensions/portable_conversation/portable_conversation.dart';
@@ -8,13 +9,29 @@ import 'package:sheason_chat/scope/scope.model.dart';
 class ChatRoomPageTitle extends StatelessWidget {
   const ChatRoomPageTitle({super.key});
 
+  Stream<String> watchUsername(Scope scope, AccountIndex agent) {
+    final select = scope.db.contacts.select();
+    select.where((tbl) => tbl.signPubkey.equals(agent.signPubKey));
+    return select.watchSingle().map((contact) {
+      final username = contact.snapshot.username;
+      if (username.isEmpty) return agent.signPubKey;
+
+      return username;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     final scope = context.watch<Scope>();
     final conversation = context.watch<Conversation>();
     if (conversation.type == ConversationType.CONVERSATION_PRIVATE) {
-      final snapshot = conversation.info.findPrivateSnapshot(scope);
-      return Text(snapshot.username);
+      final agent = conversation.info.findAgent(scope);
+
+      return StreamBuilder<String>(
+        initialData: agent.signPubKey,
+        stream: watchUsername(scope, agent),
+        builder: (context, snapshot) => Text(snapshot.requireData),
+      );
     }
 
     throw UnimplementedError();
